99ce4bda81a032bfb98f0fa2f27868860bc3cf48
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-05-26 21:20:28 -0300

    add: ambulancia van refrigerada

--- Veiculos/VanRefrigerada.java
+++ Veiculos/VanRefrigerada.java
@@ -4,19 +4,23 @@ public class VanRefrigerada extends Veiculo{
 
     private double temperaturaAtual;
     private boolean controleTemperatura;
+    
             
     public VanRefrigerada(String placa, String modelo, double capacidadeKg, double temperaturaAtual, boolean controleTemperatura) {
         super(placa, modelo, capacidadeKg);
         this.temperaturaAtual = temperaturaAtual;
         this.controleTemperatura = controleTemperatura;
+       
         
     }
 
-    public double temperaturaAtual(){
+    public double getTemperaturaAtual(){
         return temperaturaAtual;
     }
-    public boolean controleTemperatura(){
+    public boolean isControleTemperatura(){
         return controleTemperatura;
     }
+    
+    //TODO: add metodo de verificar temperatura
     
 }
