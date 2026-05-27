f9913d01843b615b24ae1a15097a638cfad542e7
rml-x <cristiane.xavier@aluno.riogrande.ifrs.edu.br>
2026-05-27 10:03:26 -0300

    add: enum nivel clinico e urgencia

---
+++ enums/NivelClinico.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum NivelClinico {
+    ESTAVEL,
+    OBSERVACAO,
+    GRAVE,
+    CRITICO
+}
---
+++ enums/Urgencia.java
@@ -0,0 +1,8 @@
+package enums;
+
+public enum Urgencia {
+    BAIXA,
+    MEDIA,
+    ALTA,
+    CRITICA
+}
